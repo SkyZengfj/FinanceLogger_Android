4d8e9aa10867d90a0bd39eb2a756813801c3981f
SkyZengfj <skyzengfj@qq.com>
2014-05-29 17:53:08 +0800

    init

---
+++ FinanceLogger/src/net/sky/app/android/financelogger/MainApplication.java
@@ -0,0 +1,7 @@
+package net.sky.app.android.financelogger;
+
+import android.app.Application;
+
+public class MainApplication extends Application {
+
+}
---
+++ FinanceLogger/src/net/sky/app/android/financelogger/ui/LockActivity.java
@@ -0,0 +1,15 @@
+package net.sky.app.android.financelogger.ui;
+
+import net.sky.app.android.financelogger.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LockActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.lock_activity);
+	}
+}
